067d158186b70b9eca1b100a7821354944ec0637
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-10-23 22:22:31 -0300

    Create Onboard screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "@/components/CustomButton";
 
 const App = () => {
   return (
-    <View className="justify-center items-center flex-1">
-      <Text className="font-pblack text-3xl">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="text-blue-500 text-xl font-semibold">Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[94vh] px-4">
+           <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+ 
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white text-center font-pbold">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image source={images.path} className="absolute top-6 right-0 w-[75px] h-20" resizeMode="contain" />
+          </View>
+          <Text className="text-gray-100 text-base text-center my-5 font-pregular">Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora</Text>
+          <CustomButton title="Continue with email" containerStyles="w-full py-4" handlePress={() => router.push("/sign-in")}/>
+        </View>
+        <StatusBar backgroundColor="#161622" style="light" />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
